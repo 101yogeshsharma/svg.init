87fc4156621763f044b55b460ef6b6b57a98e257
Yogesh Sharma <SharmaY@mscdirect.com>
2023-12-07 17:20:51 +0530

    Re. Update vite config to use absolute path instead of relative path for imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "@/App.css";
 import Navbar from "@/layout/Navbar/Navbar.jsx";
 import Customization from "@/layout/Customization/Customization.jsx";
-import Playground from "@/layout/playground/Playground.jsx";
+import Playground from "@/layout/Playground/Playground.jsx";
 import { useState } from "react";
 import Actions from "@/layout/Actions/Actions.jsx";
 
