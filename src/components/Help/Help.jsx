2f044ade9f2c73b85d2fd04838b89627a9b070e3
Sharma <yogesh.u.sharma@accenture.com>
2024-01-04 23:43:43 +0530

    Update help button style

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import "./Help.css";
 import Modal from '../Modal/Modal';
+import ClickSoundEffect from "../../assets/audio/click_sound.wav";
 
 export default function Help() {
     const [showModal, setShowModal] = useState(false);
 
     const showHideModal = (value) => {
         setShowModal(value);
+        if (value) {
+            const audio = document.getElementById("audio");
+            audio.play();
+        }
     }
     return (
         <>
-        <div className='help-handle'>
-            <div className='help-handle-button' onClick={() => showHideModal(true)}>?</div>
-        </div>
-        <Modal show={showModal} closeModal={() => showHideModal(false)}/>
+            <div className='help-handle'>
+                <div className='help-handle-button'>
+                    <div className='light-bulb' onClick={() => showHideModal(true)}><span>?</span></div>
+                    <div className='light-effect'>
+                        <audio id="audio" src={ClickSoundEffect}></audio>
+                    </div>
+                </div>
+            </div>
+            <Modal show={showModal} closeModal={() => showHideModal(false)} />
         </>
     )
 }
--- src/layout/Playground/Playground.jsx
+++ src/layout/Playground/Playground.jsx
@@ -25,14 +25,15 @@ export default function Playground({ shapeProperties, setContextMenuState, hideC
 
 	const fillColor = filled ? backgroundColor || "transparent" : "transparent";
 
+
 	return (
 		<div className='playground' id="playground" onContextMenu={handleShowContextMenu} onClick={hideContextMenu}>
-			<div className='playground-content' id='playground-content' style={{ width: '500px', height: '500px' }}>
+			<div className='playground-content' id='playground-content'>
 				<svg height="500px" width="500px">
 					{selectedShape === "square" && <rect x='120px' y='120px' rx={borderRadius} ry={borderRadius} width="200px" height="200px" fill={fillColor} stroke={strokeColor} stroke-width={strokeWidth + "px"} />}
 					{selectedShape === "circle" && <circle cx="220" cy="220" r="95" fill={fillColor} stroke={strokeColor} stroke-width={strokeWidth + "px"} />}
 					{selectedShape === "ellipse" && <ellipse cx="220" cy="220" rx="95" ry="48" fill={fillColor} stroke={strokeColor} stroke-width={strokeWidth + "px"} />}
-					{selectedShape === "line" && <line x1="130" x2="310" y1="220" y2="220" fill={fillColor} stroke={strokeColor} stroke-width={strokeWidth} stroke-linecap={roundedEdges ? "round" : "square"}/>}
+					{selectedShape === "line" && <line x1="130" x2="310" y1="220" y2="220" fill={fillColor} stroke={strokeColor} stroke-width={strokeWidth} stroke-linecap={roundedEdges ? "round" : "square"} />}
 				</svg>
 			</div>
 		</div>
